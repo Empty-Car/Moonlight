99cad6dbefcb383ed268ff119081465e608bc11e
limyeonsang <limquo@gmail.com>
2022-04-26 10:30:41 +0900

    get dates

--- src/component/EmotionDiary.js
+++ src/component/EmotionDiary.js
@@ -1,6 +1,5 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { useState, useCallback } from "react";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 const Calendar = styled.div`
   margin-top: 50px;
@@ -15,102 +14,47 @@ const Calendar = styled.div`
   transform: translate(-50%, -50%);
 `;
 
-const DayButtonBox = styled.div`
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-`;
-
-const DayButton = styled.button`
-  flex: none;
-
-  width: 150px;
-`;
-
 const EmotionDiary = () => {
-  const [year, setYear] = useState(new Date().getFullYear());
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
-  const [days, setDays] = useState({
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    8: [],
-    9: [],
-    10: [],
-    11: [],
-    12: [],
-  });
+  const DATE = new Date();
+  const YEAR = DATE.getFullYear();
+  const MONTH = DATE.getMonth() + 1;
 
-  const [dayOfWeek, setDayOfWeek] = useState({
-    0: [],
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-  });
+  const [month, setMonth] = useState(MONTH);
+  const [year, setYear] = useState(YEAR);
+  const [totalDate, setTotalDate] = useState([]);
 
-  const onUpdateDays = () => {
-    const lastDate = new Date(year, month, 0).getDate();
+  const changeDate = (month) => {
+    let prevMonthLastDate = new Date(YEAR, month - 1, 0).getDate();
+    let prevMonthLastDay = new Date(YEAR, month - 1, 0).getDay();
 
-    var dayArr = [];
-    for (var i = 1; i <= lastDate; i++) {
-      dayArr.push(i);
+    const thisMonthLastDate = new Date(YEAR, month, 0).getDate();
+    const thisMonthLastDay = new Date(YEAR, month, 0).getDay();
+
+    let prevMonthDateArr = [];
+    if (prevMonthLastDay !== 6) {
+      for (let i = 0; i < prevMonthLastDay + 1; i++) {
+        prevMonthDateArr.unshift(prevMonthLastDate - i);
+      }
     }
-    setDays({ ...days, [month]: dayArr });
+
+    let nextMonthDateArr = [];
+    if (thisMonthLastDay !== 6) {
+      for (let i = 1; i < 7 - thisMonthLastDay; i++) {
+        nextMonthDateArr.push(i);
+      }
+    }
+
+    let thisMonthDateArr = [];
+    thisMonthDateArr = [...Array(thisMonthLastDate + 1).keys()].slice(1);
+
+    return prevMonthDateArr.concat(thisMonthDateArr, nextMonthDateArr);
   };
 
-  const daysIndayOfWeek = () => {
-    var ddddArr = [];
-    for (var i = 0; i < days[month].length; i++) {
-      ddddArr.push(new Date(year, month, days[month][i]).getDay());
-    }
-    setDayOfWeek({...dayOfWeek, []})
-  };
+  useEffect(() => {
+    setTotalDate(changeDate(month));
+  }, [month]);
 
-  const onNextMonth = useCallback(() => {
-    setMonth((month) => month + 1);
-
-    if (month >= 12) {
-      setMonth(1);
-      setYear((year) => year + 1);
-    }
-
-    onUpdateDays();
-  }, [month, year]);
-
-  const onPriorMonth = useCallback(() => {
-    setMonth((month) => month - 1);
-
-    if (month <= 1) {
-      setMonth(12);
-      setYear((year) => year - 1);
-    }
-
-    onUpdateDays();
-  }, [month, year]);
-
-  return (
-    <Calendar>
-      <div>
-        <MdKeyboardArrowLeft onClick={onPriorMonth} />
-        {year}년 {month}월
-        <MdKeyboardArrowRight onClick={onNextMonth} />
-      </div>
-      <DayButtonBox>
-        {days[month].map((day, idx) => (
-          <DayButton key={idx}>{day}</DayButton>
-        ))}
-      </DayButtonBox>
-
-      <div onClick={daysIndayOfWeek}>ddddddd</div>
-    </Calendar>
-  );
+  return <Calendar></Calendar>;
 };
 
 export default EmotionDiary;
